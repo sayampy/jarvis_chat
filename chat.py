4f13cf6082331c467db3c24c1bf04150fbfb6c25
sayampy <81681676+sayampy@users.noreply.github.com>
2021-06-28 21:36:41 +0530

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from flask import request
+from jarvis_main import *
+
+app = Flask('')
+@app.route("/")
+def home():
+    return render_template("home.html")
+@app.route("/get")
+def get_bot_response():
+    update_user('user',request.remote_addr)
+    userText = request.args.get('msg')
+    return str(reply(userText,request.remote_addr))
+
+run = app.run(debug=False,host='0.0.0.0')
+t = Thread(target=run)
+t.start()
